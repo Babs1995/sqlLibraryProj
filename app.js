5df4dfe433df0fbc45d067a65098222340fe923d
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-08-16 12:53:41 -0700

    fav icon error

--- app.js
+++ app.js
@@ -1,57 +1,39 @@
-const createError = require('http-error');
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookies = require('cookie-parser')
-const router = require('./routes/index');
-const books = require('./routes/books');
-const sequelize = require('./models/index.js').sequelize;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-(async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync({ force: true });
-    
-        console.log("Connection to the database successful!");
-    
-      } catch (error) {
-        if (error.name === "SequelizeValidationError") {
-          const errors = error.errors.map((err) => err.message);
-          console.error("Connection to the database was unsuccessful.", errors);
-        }
-      }
-    
-    })();
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookies());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', router);
-app.use('/books', books);
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-app.use((err, res) => {
-  if (err) {
-    if (err.status === 404) {
-      res.status(404)
-      render(err.message, { err });
-    } else {
-      err.message = "Oh no! There was an error.";
-      res.status(500).render('error', { err });
-    }
-  }
-}); 
 
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
